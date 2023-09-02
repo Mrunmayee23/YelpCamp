d018f120594cd59bf3ac1821b287fcda01b0d7c0
Mrunmayee23 <104962928+Mrunmayee23@users.noreply.github.com>
2023-09-02 14:11:08 +0900

    Express Error app

--- app.js
+++ app.js
@@ -1,25 +1,72 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const methhodOverride = require('method-override')
 const mongoose = require('mongoose')
+const Campground = require('./models/campgrounds')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser : true,
-    useCreateIndex : true, 
-    useUnifiedTopology : true
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {useNewUrlParser : true})
+.then(() =>{
+    console.log('CONNECTION OPEN')
+})
+.catch(err =>{
+    console.log("OH NO!! ERROR!!")
+    console.log(err)
 })
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error:"));
-db.once("open", () =>{
-    console.log("Database connected")
-})
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
+app.use(express.urlencoded({extended : true}))
+app.use(methhodOverride('_method'))
+
+
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/campgrounds', catchAsync (async(req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', {campgrounds})
+}))
+
+app.get('/campgrounds/new', (req,res) =>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req, res, next) =>{
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async(req, res) =>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req, res) =>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
+
+app.use((err, req, res, next ) =>{
+    res.send('Yare Yare')
 })
 
 app.listen(3000, ()=>{

040f9a8cb8d7b3729df6f818bbadc3eef6c4a8aa
Mrunmayee <mrunmayeej.2000@gmail.com>
2023-10-07 21:30:39 +0900

    updated DbUrl link

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const campgroundsRoutes = require('./routes/campgrounds')
 const reviewsRoutes = require('./routes/reviews')
 
 //DATEBASE CONNECTION
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 //'mongodb://127.0.0.1:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser : true
